cb22bd4659b531d1eaf73409718fd48b0baa7035
Joaquim Gumucio <jjgumucio@gmail.com>
2015-10-18 19:24:51 -0300

    Find Limit exercise

---
+++ find_limit.js
@@ -0,0 +1,26 @@
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/learnyoumongo';
+
+
+mongo.connect(url, function(err, db) {
+  if ( err ) {
+    return console.error(err);
+  }
+
+  var parrots = db.collection('parrots');
+  parrots.find({
+    age: {
+      $gt: parseInt(process.argv[2])
+    }
+  }, {
+    name: 1,
+    age: 1,
+    _id: 0
+  }).toArray(function(err, docs) {
+    if ( err ) {
+      return console.error(err);
+    }
+    console.log(docs);
+    db.close();
+  });
+});
